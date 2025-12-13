0df75a0afd7df8fc2d5eb8f828d322cafec9c9af
Brett Nemec <128428098+brenem@users.noreply.github.com>
2025-12-12 20:01:58 -0500

    feat: Add Day 11 HTML structure, sample input, and initial solution functions

---
+++ solutions/2025/day11/solution.ts
@@ -0,0 +1,8 @@
+
+export async function part1(input: string): Promise<number | string | bigint> {
+    return 0;
+}
+
+export async function part2(input: string): Promise<number | string | bigint> {
+    return 0;
+}
